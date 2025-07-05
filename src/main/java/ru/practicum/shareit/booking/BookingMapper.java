c5bbb648832686da476e7f3f27374b918cfba758
Savelev_AU <andreisavelv@yandex.ru>
2025-07-05 14:08:52 +0300

    Выполнение 14ТЗ исправление замечаний ревью. добавление enum и библиотеку mapStruct (попытка №4)

--- src/main/java/ru/practicum/shareit/booking/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/BookingMapper.java
@@ -7,13 +7,15 @@ import ru.practicum.shareit.booking.dto.BookingDto;
 @Mapper(componentModel = "spring")
 public interface BookingMapper {
 
-    @Mapping(source = "dto.itemId", target = "itemId")
-    @Mapping(source = "dto.start", target = "start")
-    @Mapping(source = "dto.end", target = "end")
-    @Mapping(target = "bookerId", source = "bookerId")
-    @Mapping(target = "status", source = "status")
+    // Маппинг из BookingDto + дополнительных параметров в Booking
+    @Mapping(source = "itemId", target = "itemId")
+    @Mapping(source = "start", target = "start")
+    @Mapping(source = "end", target = "end")
+    @Mapping(source = "bookerId", target = "bookerId")
+    @Mapping(source = "status", target = "status")
     Booking toBooking(BookingDto dto, Long bookerId, BookingStatus status);
 
+    // Маппинг из Booking в BookingDto
     @Mapping(source = "id", target = "id")
     @Mapping(source = "itemId", target = "itemId")
     @Mapping(source = "start", target = "start")
