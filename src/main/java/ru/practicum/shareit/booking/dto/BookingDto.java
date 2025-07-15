deaa5ac5cf404b60d9c5f42b7704a4443d58fd70
Savelev_AU <andreisavelv@yandex.ru>
2025-07-15 07:24:27 +0300

    Выполнение 15ТЗ исправление замечаний ревью

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -2,11 +2,15 @@ package ru.practicum.shareit.booking.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.user.UserDto;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class BookingDto {
     private Long id;
     private LocalDateTime start;
@@ -21,73 +25,5 @@ public class BookingDto {
     private UserDto booker;
     private String status;
 
-    public BookingDto() {
-    }
 
-    public BookingDto(Long id, LocalDateTime start, LocalDateTime end,
-                      Long itemId, ItemDto item, UserDto booker, String status) {
-        this.id = id;
-        this.start = start;
-        this.end = end;
-        this.itemId = itemId;
-        this.item = item;
-        this.booker = booker;
-        this.status = status;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getStart() {
-        return start;
-    }
-
-    public void setStart(LocalDateTime start) {
-        this.start = start;
-    }
-
-    public LocalDateTime getEnd() {
-        return end;
-    }
-
-    public void setEnd(LocalDateTime end) {
-        this.end = end;
-    }
-
-    public Long getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(Long itemId) {
-        this.itemId = itemId;
-    }
-
-    public ItemDto getItem() {
-        return item;
-    }
-
-    public void setItem(ItemDto item) {
-        this.item = item;
-    }
-
-    public UserDto getBooker() {
-        return booker;
-    }
-
-    public void setBooker(UserDto booker) {
-        this.booker = booker;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
